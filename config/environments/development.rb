81d5889613878d80cb73fec317fa330a470d5d5e
Ayokunnumi1 <omololuayk@gmail.com>
2024-10-08 23:34:43 +0100

    Enable ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "0431-105-112-70-135.ngrok-free.app"
+ config.hosts << "a1f8-105-112-70-135.ngrok-free.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time

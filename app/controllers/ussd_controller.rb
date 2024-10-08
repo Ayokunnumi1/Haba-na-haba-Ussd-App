235d582f0d26201a657576dc298d354e638d77b1
Ayokunnumi1 <omololuayk@gmail.com>
2024-10-08 23:33:29 +0100

    Add ussd controller to test africastalking

---
+++ app/controllers/ussd_controller.rb
@@ -0,0 +1,30 @@
+class UssdController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    session_id = params[:sessionId]
+    phone_number = params[:phoneNumber]
+    text = params[:text]
+    service_code = params[:serviceCode]
+
+    response = process_ussd(text)
+
+    render plain: response
+  end
+
+  private
+
+  def process_ussd(text)
+    case text
+    when ""
+      
+      "CON Welcome to Haba na haba\n1. Donate Food\n2. Request Food"
+    when "1"
+      "END Thank you for donating food"
+    when "2"
+      "END We would send you food shorthly"
+    else
+      "END Invalid choice"
+    end
+  end
+end

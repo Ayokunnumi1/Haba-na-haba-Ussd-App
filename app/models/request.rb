d5098b0923e119676e296bcf306d5479c53460ad
Al-Amin <alaminkhanshakil@gmail.com>
2024-10-09 17:52:40 +0600

    Create request model

---
+++ app/models/request.rb
@@ -0,0 +1,10 @@
+class Request < ApplicationRecord
+  belongs_to :district
+  belongs_to :county
+  belongs_to :sub_county
+  belongs_to :branch
+  belongs_to :user, optional: true
+
+  validates :name, :phone_number, :request_type, :residence_address, :village, :parish, presence: true
+  validates :phone_number, format: { with: /\A\d+\z/, message: "only allows numbers" }
+end

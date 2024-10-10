4bc0abc8814b91aba1b8e956436e112e4881c53f
Al-Amin <alaminkhanshakil@gmail.com>
2024-10-10 20:15:15 +0600

    Redefine the validations

--- app/models/county.rb
+++ app/models/county.rb
@@ -5,4 +5,6 @@ class County < ApplicationRecord
   has_many :family_beneficiaries, dependent: :nullify
   has_many :organization_beneficiaries, dependent: :nullify
   has_many :inventories, dependent: :nullify
+
+  validates :name, presence: true
 end

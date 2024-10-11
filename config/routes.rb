23557b8e3fc5e8d5b5dbb983a729fc1f7107fbba
Al-Amin <alaminkhanshakil@gmail.com>
2024-10-12 01:10:39 +0600

    Define user route for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, skip: [:registrations]
 
-  root to: 'users#index'
+  # Conditional root route
+  authenticated :user do
+    root to: 'users#index', as: :authenticated_root
+  end
 
-  resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
+  unauthenticated do
+    root to: 'home#index', as: :unauthenticated_root
+  end
+
+  resources :users
 end

41c1a4dbac819b7688bee776a9279452fd9cefdd
Austin Obimbo <obimboausts@gmail.com>
2024-10-16 17:32:57 +0300

    updated country, request, district validations

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,5 +1,5 @@
 class District < ApplicationRecord
-  has_many :counties
+  has_many :counties, dependent: :destroy
   has_many :individual_beneficiaries, dependent: :nullify
   has_many :family_beneficiaries, dependent: :nullify
   has_many :organization_beneficiaries, dependent: :nullify
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,14 +1,15 @@
 class Request < ApplicationRecord
-  belongs_to :district
-  belongs_to :county
-  belongs_to :sub_county
-  belongs_to :branch
+  belongs_to :district, optional: true
+  belongs_to :county, optional: true
+  belongs_to :sub_county, optional: true
+  belongs_to :branch, optional: true
   belongs_to :user, optional: true
+
   has_one :individual_beneficiary, dependent: :destroy
   has_one :family_beneficiary, dependent: :destroy
   has_one :organization_beneficiary, dependent: :destroy
   has_many :inventories, dependent: :nullify
 
-  validates :name, :phone_number, :request_type, :residence_address, :village, :parish, presence: true
+  validates :name, :phone_number, presence: true
   validates :phone_number, format: { with: /\A[\d+]+\z/, message: 'only allows numbers' }
 end

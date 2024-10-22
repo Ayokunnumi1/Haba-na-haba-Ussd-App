44d330aa960f9f19ab88d030a41f2d7937cd2c01
Al-Amin <alaminkhanshakil@gmail.com>
2024-10-23 02:58:19 +0600

    Add organization beneficiaries controller

--- app/controllers/organization_beneficiaries_controller.rb
+++ app/controllers/organization_beneficiaries_controller.rb
@@ -1,13 +1,108 @@
 class OrganizationBeneficiariesController < ApplicationController
+  before_action :set_request, only: %i[new create edit update]
+  before_action :set_organization_beneficiary, only: %i[edit update show destroy]
+
   def index
+    @organization_beneficiaries = OrganizationBeneficiary.includes(:request).all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    if @request.organization_beneficiary.present?
+      redirect_to organization_beneficiaries_path, alert: 'Organization Beneficiary already exists for this request.'
+    else
+      @organization_beneficiary = @request.build_organization_beneficiary
+      @districts = District.all
+      @counties = County.none
+      @sub_counties = SubCounty.none
+    end
+  end
+
+  def create
+    if @request.organization_beneficiary.present?
+      redirect_to @request, alert: 'An Organization Beneficiary already exists for this request.'
+    else
+      @organization_beneficiary = @request.build_organization_beneficiary(organization_beneficiary_params)
+      if @organization_beneficiary.save
+        redirect_to @organization_beneficiary, notice: 'Organization Beneficiary was successfully created.'
+      else
+        render :new
+      end
+    end
   end
 
   def edit
+    @districts = District.all
+    @counties = if @organization_beneficiary.district.present?
+                  County.where(district_id: @organization_beneficiary.district_id)
+                else
+                  County.none
+                end
+    @sub_counties = if @organization_beneficiary.county.present?
+                      SubCounty.where(county_id: @organization_beneficiary.county_id)
+                    else
+                      SubCounty.none
+                    end
+  end
+
+  def update
+    if @organization_beneficiary.update(organization_beneficiary_params)
+      redirect_to organization_beneficiaries_path, notice: 'Organization Beneficiary was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @organization_beneficiary.destroy
+      redirect_to organization_beneficiaries_path, notice: 'Organization Beneficiary was successfully deleted.'
+    else
+      redirect_to organization_beneficiaries_path, alert: 'Failed to delete Organization Beneficiary.'
+    end
+  rescue StandardError => e
+    redirect_to organization_beneficiaries_path, alert: handle_destroy_error(e)
+  end
+
+  def load_counties
+    @counties = if params[:district_id].present?
+                  County.where(district_id: params[:district_id])
+                else
+                  County.none
+                end
+    render json: @counties.map { |county| { id: county.id, name: county.name } }
+  end
+
+  def load_sub_counties
+    @sub_counties = if params[:county_id].present?
+                      SubCounty.where(county_id: params[:county_id])
+                    else
+                      SubCounty.none
+                    end
+    render json: @sub_counties.map { |sub_county| { id: sub_county.id, name: sub_county.name } }
+  end
+
+  private
+
+  def set_request
+    @request = Request.find(params[:request_id]) if params[:request_id]
+  end
+
+  def set_organization_beneficiary
+    if params[:request_id]
+      @request = Request.find(params[:request_id])
+      @organization_beneficiary = @request.organization_beneficiary
+    else
+      @organization_beneficiary = OrganizationBeneficiary.find(params[:id])
+    end
+  end
+
+  def organization_beneficiary_params
+    params.require(:organization_beneficiary).permit(
+      :organization_name, :male, :female, :adult_age_range, :children_age_range, :district_id, :county_id,
+      :sub_county_id, :residence_address, :village, :parish, :phone_number, :case_name, :case_description,
+      :registration_no, :organization_no, :directors_name, :head_of_institution, :number_of_meals_home, :basic_FEH,
+      :request_id
+    )
   end
 end
--- test/controllers/organization_beneficiaries_controller_test.rb
+++ test/controllers/organization_beneficiaries_controller_test.rb
@@ -1,22 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class OrganizationBeneficiariesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get organization_beneficiaries_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get organization_beneficiaries_show_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get organization_beneficiaries_new_url
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get organization_beneficiaries_edit_url
     assert_response :success
   end

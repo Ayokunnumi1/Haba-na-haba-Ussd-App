2c67164f0ca8f81737e59657f70363834e86629e
Ayokunnumi1 <omololuayk@gmail.com>
2024-10-22 15:03:06 +0100

    Refactor impact section styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# app/helpers/application_helper.rb
+
 module ApplicationHelper
+  def impact_item(image, size, alt, title, description)
+    content_tag :div, class: "impact-grid-item" do
+      concat image_tag(image, size: size, alt: alt)
+      concat content_tag(:h3, title, class: "font-bold text-xl")
+      concat content_tag(:p, description, class: "font-normal text-sm")
+    end
+  end
 end

797a6a6d2f647ab419917908d2a61cc84ce5470e
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-10-25 23:14:20 +0600

    refactored code

---
+++ app/helpers/impact_helper.rb
@@ -0,0 +1,27 @@
+module ImpactHelper
+    def icon_name(index)
+      icons = ["Clock", "Calender", "Pie Chart", "Handshake"]
+      icons[index]
+    end
+  
+    def impact_value(index)
+      values = ["240+", "1000 M TONS", "6000 KG", "72+"]
+      values[index]
+    end
+  
+    def impact_label(index)
+      labels = ["Food Donations", "Current Year", "Current Year food collection", "Partnerships"]
+      labels[index]
+    end
+  
+    def impact_item(icon, size, name, value, label)
+      <<-HTML.html_safe
+        <div class="flex flex-col items-center text-center p-4">
+          <img src="/assets/#{icon}" alt="#{name}" class="w-#{size} h-#{size} mb-2"> <!-- Adjust the margin-bottom -->
+          <p class="font-bold text-xl sm:text-2xl lg:text-3.8xl mb-1">#{value}</p> <!-- Adjust the margin-bottom -->
+          <p class="font-normal text-sm sm:text-base lg:text-lg">#{label}</p>
+        </div>
+      HTML
+    end
+  end
+  

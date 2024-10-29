c341ace3eceee217c2b56df95c188714ff2080fe
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-10-29 14:54:42 +0600

    linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@
 
 module ApplicationHelper
   def impact_item(image, size, alt, title, description)
-    content_tag :div, class: "impact-grid-item" do
+    content_tag :div, class: 'impact-grid-item' do
       concat image_tag(image, size: size, alt: alt)
-      concat content_tag(:h3, title, class: "font-bold text-xl lg:text-3.8xl lg:mt-4")
-      concat content_tag(:p, description, class: "font-normal text-sm lg:text-base lg:mt-4")
+      concat content_tag(:h3, title, class: 'font-bold text-xl lg:text-3.8xl lg:mt-4')
+      concat content_tag(:p, description, class: 'font-normal text-sm lg:text-base lg:mt-4')
     end
   end
 end

368c0fb10fb2a275933d182e8fd5ca90049a5398
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-10-29 14:51:07 +0600

    linters issues solved

--- app/helpers/impact_helper.rb
+++ app/helpers/impact_helper.rb
@@ -16,10 +16,10 @@ module ImpactHelper
 
   def impact_item(icon, size_classes, name, value, label)
     <<-HTML.html_safe
-        <div class="flex flex-col items-center text-center p-4">
-          <img src="/assets/#{icon}" alt="#{name}" class="#{size_classes} mb-2"> <!-- Adjust the margin-bottom -->
-          <p class="font-bold text-lg sm:text-2xl lg:text-3.8xl mb-1">#{value}</p> <!-- Adjust the margin-bottom -->
-          <p class="font-normal text-xs sm:text-base lg:text-lg">#{label}</p>
+        <div class='flex flex-col items-center text-center p-4'>
+          <img src='/assets/#{icon}' alt='#{name}' class='#{size_classes} mb-2'> <!-- Adjust the margin-bottom -->
+          <p class='font-bold text-lg sm:text-2xl lg:text-3.8xl mb-1'>#{value}</p> <!-- Adjust the margin-bottom -->
+          <p class='font-normal text-xs sm:text-base lg:text-lg'>#{label}</p>
         </div>
     HTML
   end

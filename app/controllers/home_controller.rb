e2e55f2781dbc3cee35c87439416bd3b124196c0
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-10-29 20:00:24 +0600

    updated layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
 class HomeController < ApplicationController
+  layout 'home', only: [:index]
   def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:registrations]
 
+  root 'home#index'
   get 'home/index'
   resources :users
   resources :districts

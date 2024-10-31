42b02016739cd2a388184d836b025d07a1763588
Al-Amin <alaminkhanshakil@gmail.com>
2024-10-31 13:16:03 +0600

    Has many baranches relations are added

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,5 +1,6 @@
 class District < ApplicationRecord
   has_many :counties
+  has_many :branches
   has_many :individual_beneficiaries, dependent: :nullify
   has_many :family_beneficiaries, dependent: :nullify
   has_many :organization_beneficiaries, dependent: :nullify

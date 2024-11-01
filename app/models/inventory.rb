07dffcfe9e5e8e65cf90a37fc3644111b2927e71
Austin Obimbo <obimboausts@gmail.com>
2024-11-01 11:35:41 +0300

    index view design and functionality

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -8,12 +8,25 @@ class Inventory < ApplicationRecord
   validates :phone_number, :amount, presence: true
   validates :phone_number, format: { with: /\A[\d+]+\z/, message: 'only allows numbers' }
 
-  scope :by_donation_type, ->(type) { where(donation_type: type) if type.present? }
-  scope :by_donation_date, ->(date) { where(donation_date: date) if date.present? }
-  scope :by_collection_range, ->(start_date, end_date) {
-    where(collection_date: start_date..end_date) if start_date.present? && end_date.present?
+  scope :by_food_type, -> { where(donor_type: ['dry_food', 'fresh_food', 'other']) }
+  scope :donated, -> { joins(:request).where(requests: { request_type: 'food' }) }
+  scope :stock_alert, -> { joins(:request).where(requests: { is_selected: true }) }
+  scope :expired, -> { where("expire_date < ?", Date.today) }
+
+  scope :by_donation_type, ->(type) { where(donor_type: type) if type.present? }
+  scope :by_donation_date, ->(date) { where(collection_date: date) if date.present? }
+
+  scope :by_expire_range, ->(start_date, end_date) {
+    where(expire_date: start_date..end_date) if start_date.present? && end_date.present?
   }
-  scope :by_collection_amount, ->(amount) { where("collection_amount >= ?", amount) if amount.present? }
+
+  scope :search_query, ->(query) {
+    where("food_name ILIKE :query OR donor_name ILIKE :query", query: "%#{query}%") if query.present?
+  }
+  
+  scope :by_collection_amount, ->(min_amount, max_amount) {
+    where(amount: min_amount..max_amount) if min_amount.present? && max_amount.present?
+  }
 
   scope :search, ->(query) {
     where("food_name ILIKE :query OR donor_name ILIKE :query", query: "%#{query}%")

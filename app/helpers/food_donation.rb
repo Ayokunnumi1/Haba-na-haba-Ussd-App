b02aa16c1fbd89fb81f5e6b4081f01acc62c093e
Austin Obimbo <obimboausts@gmail.com>
2024-11-11 18:41:48 +0300

    ussd request modules

---
+++ app/helpers/food_donation.rb
@@ -0,0 +1,59 @@
+module FoodDonationModule
+    def self.process_request(text, phone_number, session)
+      input_parts = text.split('*')
+      request_name = input_parts[1]
+      district_name = input_parts[2]
+      county_name = input_parts[3]
+      sub_county_name = input_parts[4]
+      food_name = input_parts[5]
+      amount = input_parts[6]
+  
+      # Locate the district and branch
+      selected_district = District.search_by_name(district_name).first
+      selected_county = County.search_by_name(county_name).first
+      selected_sub_county = SubCounty.search_by_name(sub_county_name).first
+  
+      return 'END No matching district found.' if selected_district.nil?
+      return 'END No matching county found.' if selected_county.nil?
+      return 'END No matching sub county found.' if selected_sub_county.nil?
+  
+  
+  
+      puts "Selected district #{selected_district}"
+      puts "Selected District: ID=#{selected_district.id}, Name=#{selected_district.name}"
+  
+      branch = Branch.joins(:districts).find_by(districts: { id: selected_district.id },)
+      
+      return 'END No matching district found.' if branch.nil?
+  
+      puts "Branch found: #{branch&.id}"
+      puts "Request data: #{phone_number}, #{session[:name]}, #{selected_district&.id}, #{selected_county&.id}, #{selected_sub_county&.id}, #{branch&.id}"
+  
+      
+      # Create donation request
+      request = Request.create(
+        phone_number:,
+        name: request_name,
+        request_type: 'Donation',
+        is_selected: true,
+        district_id: selected_district.id,
+        county_id: selected_county.id,
+        sub_county_id: selected_sub_county.id,
+        branch_id: branch.id,
+      )
+  
+      # Create inventory record for the donation
+      Inventory.create(
+        donor_name: request.name,
+        food_name:,
+        amount:,
+        district_id: selected_district.id,
+        county_id: selected_county.id,
+        sub_county_id: selected_sub_county.id,
+        phone_number:,
+        request_id: request.id,
+      )
+      'END Thank you for your donation we are reaching out to you shortly'
+    end
+  end
+  
---
+++ app/helpers/food_request.rb
@@ -0,0 +1,53 @@
+module FoodRequestModule
+    def self.process_request(text, phone_number, session, name)
+      all_districts = District.all
+      puts 'All Districts:'
+      all_districts.each do |district|
+        puts "District ID: #{district.id}, Name: #{district.name}"
+      end
+  
+      input_parts = text.split('*')
+      request_name = text.split[1]
+      district_name = input_parts[2]
+      county_name = input_parts[3]
+      sub_county_name = input_parts[4]
+  
+      puts "district name #{district_name}"
+  
+      selected_district = District.search_by_name(district_name).first
+      selected_county = County.search_by_name(county_name).first
+      selected_sub_county = SubCounty.search_by_name(sub_county_name).first
+  
+      return 'END No matching district found.' if selected_district.nil?
+  
+      puts "Selected district #{selected_district}"
+      puts "Selected county #{selected_county}"
+      puts "Selected sub county #{selected_sub_county}"
+      puts "Selected District: ID=#{selected_district.id}, Name=#{selected_district.name}"
+      puts "Selected District: ID=#{selected_county.id}, Name=#{selected_county.name}"
+      puts "Selected District: ID=#{selected_sub_county.id}, Name=#{selected_sub_county.name}"
+  
+  
+      branch = Branch.joins(:districts).find_by(districts: { id: selected_district.id })
+      
+     
+      new_request = Request.create(
+        phone_number:,
+        name: request_name,
+        request_type: 'Food',
+        is_selected: true,
+        district_id: selected_district.id,
+        county_id: selected_county.id,
+        sub_county_id: selected_sub_county.id,  
+        branch_id: branch.id, 
+      )
+      
+      if new_request.save(validate: false)
+        'END We are processing your request and will contact you shortly.'
+      else
+        puts "Request creation failed: #{new_request.errors.full_messages.join(', ')}"
+        'END There was an issue processing your request. Please try again later.'
+      end
+    end
+  end
+  

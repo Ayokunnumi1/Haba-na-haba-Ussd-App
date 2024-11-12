e4e24bdd7b12f051e59fa1a6061f11068aa15f44
Ayokunnumi1 <omololuayk@gmail.com>
2024-11-12 02:33:18 +0100

    Fix linter error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,11 @@ module ApplicationHelper
 
   def form_field(form, field_type, field_name, options = {})
     label_class = 'block mb-2 text-sm font-medium text-gray-900'
-    input_class = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-lightGreen focus:border-lightGreen block w-full p-2.5'
+    input_class = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                   focus:ring-lightGreen focus:border-lightGreen block w-full p-2.5'
 
-    form.label(field_name, class: label_class) + send("form_#{field_type}", form, field_name, options.merge(class: input_class))
+    form.label(field_name,
+               class: label_class) + send("form_#{field_type}", form, field_name, options.merge(class: input_class))
   end
 
   private
@@ -33,11 +35,13 @@ module ApplicationHelper
   end
 
   def form_file_field(form, field_name, options)
-    file_input_class = 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none'
+    file_input_class = 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg
+                        cursor-pointer bg-gray-50 focus:outline-none'
     form.file_field(field_name, options.merge(class: file_input_class))
   end
 
   def form_collection_select(form, field_name, options)
-    form.collection_select(field_name, options[:collection], options[:value_method], options[:text_method], { prompt: options[:prompt] }, options)
+    form.collection_select(field_name, options[:collection], options[:value_method], options[:text_method],
+                           { prompt: options[:prompt] }, options)
   end
 end

bc00fed6f8ceda17a26b9cc06c2a38db9395b24d
Ayokunnumi1 <omololuayk@gmail.com>
2024-11-12 02:26:21 +0100

    Fix cyclomatic complexities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,32 +7,37 @@ module ApplicationHelper
     label_class = 'block mb-2 text-sm font-medium text-gray-900'
     input_class = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-lightGreen focus:border-lightGreen block w-full p-2.5'
 
-    case field_type
-    when :text_field
-      form.label(field_name, class: label_class) +
-        form.text_field(field_name, options.merge(class: input_class))
-    when :email_field
-      form.label(field_name, class: label_class) +
-        form.email_field(field_name, options.merge(class: input_class))
-    when :password_field
-      form.label(field_name, class: label_class) +
-        form.password_field(field_name, options.merge(class: input_class))
-    when :telephone_field
-      form.label(field_name, class: label_class) +
-        form.telephone_field(field_name, options.merge(class: input_class))
-    when :select
-      form.label(field_name, class: label_class) +
-        form.select(field_name, options[:choices], { prompt: options[:prompt] }, class: input_class)
-    when :file_field
-      form.label(field_name, class: label_class) +
-        form.file_field(field_name,
-                        class: 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none')
-    when :collection_select
-      form.label(field_name, class: label_class) +
-        form.collection_select(field_name, options[:collection], options[:value_method], options[:text_method],
-                               { prompt: options[:prompt] }, class: input_class)
-    else
-      ''
-    end
+    form.label(field_name, class: label_class) + send("form_#{field_type}", form, field_name, options.merge(class: input_class))
+  end
+
+  private
+
+  def form_text_field(form, field_name, options)
+    form.text_field(field_name, options)
+  end
+
+  def form_email_field(form, field_name, options)
+    form.email_field(field_name, options)
+  end
+
+  def form_password_field(form, field_name, options)
+    form.password_field(field_name, options)
+  end
+
+  def form_telephone_field(form, field_name, options)
+    form.telephone_field(field_name, options)
+  end
+
+  def form_select(form, field_name, options)
+    form.select(field_name, options[:choices], { prompt: options[:prompt] }, options)
+  end
+
+  def form_file_field(form, field_name, options)
+    file_input_class = 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none'
+    form.file_field(field_name, options.merge(class: file_input_class))
+  end
+
+  def form_collection_select(form, field_name, options)
+    form.collection_select(field_name, options[:collection], options[:value_method], options[:text_method], { prompt: options[:prompt] }, options)
   end
 end

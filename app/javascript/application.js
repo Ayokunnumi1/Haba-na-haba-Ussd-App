b5d7e1d924de984d9cdf1d5e286554af48d1702c
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-11-12 22:51:47 +0600

    Add search option for user

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,18 @@ import "./controllers";
 import "./controllers/load_counties";
 import "./controllers/load_sub_counties";
 import "flowbite"
+import 'select2/dist/css/select2.min.css';
+import $ from 'jquery';
+import 'select2';
+
+
+window.jQuery = $;
+window.$ = $;
+
+$(document).on('turbolinks:load', function() {
+  // Initialize select2 for the user select element
+  $('#user_ids').select2({
+    placeholder: 'Select users',
+    allowClear: true
+  });
+});

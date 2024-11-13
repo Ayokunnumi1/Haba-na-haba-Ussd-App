2000b86c44d370ef61fcd2eb3f03dfa42bdb6828
Al-Amin <alaminkhanshakil@gmail.com>
2024-11-13 11:50:06 +0600

    Edit route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, skip: [:registrations]
 
   get 'home/index'
-  get 'filter_results', to: 'your_controller#filter_results'
   resources :users
   resources :districts
   resources :counties

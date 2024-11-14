2fdcaed049f4a5cf0fdd43a0c21dd4bca4578621
Ayokunnumi1 <omololuayk@gmail.com>
2024-11-14 23:33:48 +0100

    Update truncate.js

--- app/javascript/truncate.js
+++ app/javascript/truncate.js
@@ -1,24 +1,37 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    function truncateText(element, maxLength) {
-      if (element.textContent.length > maxLength) {
-        element.textContent = element.textContent.substring(0, maxLength) + '..';
+document.addEventListener("DOMContentLoaded", function () {
+  function truncateText(element, maxLength) {
+    if (element.textContent.length > maxLength) {
+      element.textContent = element.textContent.substring(0, maxLength) + "..";
+    }
+  }
+
+  function applyTruncation() {
+    const screenWidth = window.innerWidth;
+    const maxLength = 5;
+
+    // Truncate first names
+    const firstNameElements = document.querySelectorAll(
+      '[id^="user-first-name-"]'
+    );
+    firstNameElements.forEach(function (element) {
+      if (screenWidth <= 320) {
+        truncateText(element, maxLength);
+      } else {
+        element.textContent = element.getAttribute("data-full-name");
       }
-    }
+    });
 
-    function applyTruncation() {
-      const screenWidth = window.innerWidth;
-      const maxLength = 5;
-      const elements = document.querySelectorAll('[id^="user-first-name-"]');
+    // Truncate roles
+    const roleElements = document.querySelectorAll('[id^="user-role-"]');
+    roleElements.forEach(function (element) {
+      if (screenWidth <= 320) {
+        truncateText(element, maxLength);
+      } else {
+        element.textContent = element.getAttribute("data-role-full");
+      }
+    });
+  }
 
-      elements.forEach(function(element) {
-        if (screenWidth <= 320) {
-          truncateText(element, maxLength);
-        } else {
-          element.textContent = element.getAttribute('data-full-name');
-        }
-      });
-    }
-
-    applyTruncation();
-    window.addEventListener('resize', applyTruncation);
-  });
+  applyTruncation();
+  window.addEventListener("resize", applyTruncation);
+});

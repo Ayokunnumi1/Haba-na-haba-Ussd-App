113118950425f81a0dad3b580cfe1eb631e6f3de
Ayokunnumi1 <omololuayk@gmail.com>
2024-11-19 12:06:39 +0100

    Add turbo load in filter users js functionality

--- app/javascript/filterUsers.js
+++ app/javascript/filterUsers.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("turbo:load", function () {
   const dropdownItems = document.querySelectorAll("#dropdownId2 [data-role]");
   const userElements = document.querySelectorAll(
     ".users-grid [data-users-type]"

5c2e2fe97a6013fe02cee05594adbdb16fdf4376
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-11-21 19:17:01 +0600

    fixed login bg tailwind config file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,9 +10,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'login-bg': "url('/assets/Shape.svg')",
-      },
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
         darkBlue: "rgb(48, 44, 81)",
@@ -35,6 +32,7 @@ module.exports = {
       backgroundImage: {
         "users-image": "url('/assets/users-image.svg')",
         "users-show": "url('/assets/users-show.svg')",
+        'login-bg': "url('/assets/Shape.svg')",
       },
       screens: {
         mmd: "370px",

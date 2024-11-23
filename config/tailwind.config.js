fcbd134bcd01ae683bb448d348b2d8a992a0f71e
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-11-23 15:53:43 +0600

    Scrollbar and sidebar design updated

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -44,16 +44,7 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/container-queries"),
     require("flowbite/plugin"),
-    function ({ addUtilities }) {
-      addUtilities({
-        '.no-scrollbar': {
-          '-ms-overflow-style': 'none', 
-          'scrollbar-width': 'none',
-        },
-        '.no-scrollbar::-webkit-scrollbar': {
-          display: 'none',
-        },
-      });
-    },
+    require('tailwind-scrollbar'),
+    
   ],
 };

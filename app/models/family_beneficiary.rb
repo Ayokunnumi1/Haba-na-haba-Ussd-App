cd713c930c2ebc4953a34848ed58b57cea345319
Al-Amin-Khan-Shakil <alaminkhanshakil@gmail.com>
2024-11-25 07:07:52 -0700

    Make changes for Family Beneficiary table

--- app/models/family_beneficiary.rb
+++ app/models/family_beneficiary.rb
@@ -4,11 +4,13 @@ class FamilyBeneficiary < ApplicationRecord
   belongs_to :district
   belongs_to :county
   belongs_to :sub_county
-  belongs_to :request
+  belongs_to :request, optional: true
+  belongs_to :branch, optional: true
 
   validates :family_members, :male, :female, :children, presence: true
   validates :residence_address, :village, :parish, :phone_number, presence: true
   validates :case_name, :case_description, :fathers_name, :mothers_name, :fathers_occupation, :mothers_occupation,
             presence: true
   validates :phone_number, format: { with: /\A[\d+]+\z/, message: 'only allows numbers' }
+  validates :provided_food, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_nil: true
 end

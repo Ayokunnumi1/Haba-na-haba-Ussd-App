7ab8d10cf3314e856eeb60f232e4d38ef25cb389
Austin Obimbo <obimboausts@gmail.com>
2024-11-27 02:20:37 +0300

    implemented filter scopes

--- app/models/request.rb
+++ app/models/request.rb
@@ -7,10 +7,29 @@ class Request < ApplicationRecord
   has_one :individual_beneficiary, dependent: :destroy
   has_one :family_beneficiary, dependent: :destroy
   has_one :organization_beneficiary, dependent: :destroy
-  has_many :inventories, dependent: :nullify
+  has_many :inventories, dependent: :destroy
 
   validates :name, :phone_number, :request_type, presence: true
   validates :phone_number, format: { with: /\A[\d+]+\z/, message: 'only allows numbers' }
 
   scope :by_request_type, ->(type) { where(request_type: type) if type.present? }
+
+  scope :search_query, lambda { |query|
+    if query.present?
+      joins(:inventories).where(
+        'donor_name ILIKE :query OR inventories.donor_type ILIKE :query',
+        query: "%#{query}%"
+      ).distinct
+    end
+  }
+
+  scope :by_donation_type, ->(donation_type) {
+    joins(:inventories).where(inventories: { donor_type: donation_type }) if donation_type.present?
+  }
+
+  # Scope for filtering by donation date range
+  scope :by_donation_date, ->(start_date, end_date) {
+    where(created_at: start_date..end_date) if start_date.present? && end_date.present?
+  }
+
 end

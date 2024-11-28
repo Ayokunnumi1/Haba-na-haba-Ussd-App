f439d23948ddc35223e5e78835d0e9ed56da3deb
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-11-28 20:43:19 +0600

    initial stage dynamic form feature achievement

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,7 @@ import "./controllers/load_sub_counties";
 import "requestDropdown";
 import "./controllers/toggleFilter";
 import "./dropdown";
+
 import "flowbite";
 import "truncate";
 import "./users/filterUsers";

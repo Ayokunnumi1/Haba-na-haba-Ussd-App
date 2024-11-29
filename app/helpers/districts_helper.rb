2e479b8d2e88080c7556e684f288a46e36e14a42
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-11-29 18:17:27 +0600

    text matching design 1

--- app/helpers/districts_helper.rb
+++ app/helpers/districts_helper.rb
@@ -19,23 +19,24 @@ module DistrictsHelper
   # Render the inline fields for counties
   def render_inline_county_fields(builder)
     content_tag(:div, class: "nested-fields") do
-      concat builder.label :name, "County Name"
+      concat builder.label :name,"County Name", class: "block text-gray-700 text-sm font-bold mb-2" 
       concat builder.text_field :name, class: "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      concat content_tag(:h4, "Sub-Counties") do
+      concat builder.check_box :_destroy
+      concat builder.label :_destroy, "Remove County", class: "text-red-500 ml-2"
+      concat content_tag(:h4, "Sub-Counties" , class:"text-xl font-bold mb-4") do
         builder.fields_for :sub_counties do |sub_county_fields|
           render_inline_sub_county_fields(sub_county_fields)
         end
       end
       concat link_to_add_fields("Add Sub-County", builder, :sub_counties)
-      concat builder.check_box :_destroy
-      concat builder.label :_destroy, "Remove County", class: "text-red-500 ml-2"
+      
     end
   end
 
   # Render the inline fields for sub-counties
   def render_inline_sub_county_fields(builder)
     content_tag(:div, class: "nested-fields") do
-      concat builder.label :name, "Sub-County Name"
+      concat builder.label :name, "Sub-County Name" ,class: "block text-gray-700 text-sm font-bold mb-2"
       concat builder.text_field :name, class: "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       concat builder.check_box :_destroy
       concat builder.label :_destroy, "Remove Sub-County", class: "text-red-500 ml-2"

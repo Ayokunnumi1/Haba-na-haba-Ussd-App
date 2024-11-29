e4a4b0185fe3271fd36acc545188f15ee491cb88
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-11-29 10:41:22 +0600

    Use district helper instead of application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,4 @@ module ApplicationHelper
     controller_name == 'users' && action_name.in?(%w[new edit show])
     !(controller_name == 'requests' && action_name.in?(%w[new edit show]))
   end
-
-  def link_to_add_fields(name, form, association)
-    new_object = form.object.send(association).build
-    id = new_object.object_id
-    fields = form.fields_for(association, new_object, child_index: id) do |builder|
-      render("#{association.to_s.singularize}_fields", f: builder)
-    end
-    link_to(name, '#', class: "add_fields", data: { association: association, fields: fields.delete("\n") })
-  end
 end
--- app/helpers/districts_helper.rb
+++ app/helpers/districts_helper.rb
@@ -1,4 +1,10 @@
 module DistrictsHelper
-   
-  
+    def link_to_add_fields(name, form, association)
+        new_object = form.object.send(association).build
+        id = new_object.object_id
+        fields = form.fields_for(association, new_object, child_index: id) do |builder|
+          render("#{association.to_s.singularize}_fields", f: builder)
+        end
+        link_to(name, '#', class: "add_fields", data: { association: association, fields: fields.delete("\n") })
+      end
 end

29d132de4f7257347a134665dac7f478576829ce
Al-Amin <alaminkhanshakil@gmail.com>
2024-12-02 17:17:15 +0600

    Style filter modal form

--- app/javascript/controllers/filter_modal.js
+++ app/javascript/controllers/filter_modal.js
@@ -4,19 +4,16 @@ document.addEventListener("turbo:load", () => {
   const closeModalButton = document.getElementById("close-modal-button");
   const resetFilterButton = document.getElementById("reset-filter-button");
 
-  // Open modal
   filterModalButton.addEventListener("click", () => {
     filterModal.classList.remove("hidden");
     filterModal.classList.add("flex");
   });
 
-  // Close modal
   closeModalButton.addEventListener("click", () => {
     filterModal.classList.add("hidden");
     filterModal.classList.remove("flex");
   });
 
-  // Reset filters
   resetFilterButton.addEventListener("click", () => {
     document
       .querySelectorAll("#filter-modal input, #filter-modal select")
@@ -25,7 +22,6 @@ document.addEventListener("turbo:load", () => {
       });
   });
 
-  // Close modal on background click
   filterModal.addEventListener("click", (event) => {
     if (event.target === filterModal) {
       filterModal.classList.add("hidden");

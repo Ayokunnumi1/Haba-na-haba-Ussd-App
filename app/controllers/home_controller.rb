6f47167c5f3346d0030e91bedbd40e243d950675
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-08 11:56:45 +0600

    Request connected to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,40 @@
 class HomeController < ApplicationController
   layout 'home', only: [:index]
-  def index; end
-  layout 'home', only: [:index]
+
+  def index
+    @request = Request.new
+    @branches = Branch.all
+    @districts = District.all
+    @counties = County.none
+    @sub_counties = SubCounty.none
+  end
+
+  def edit
+    @districts = District.all
+    @counties = @request.district.present? ? County.where(district_id: @request.district_id) : County.none
+    @sub_counties = @request.county.present? ? SubCounty.where(county_id: @request.county_id) : SubCounty.none
+    @branches = Branch.all
+  end
+
+  def create_request
+    @request = Request.new(request_params)
+
+    if @request.save
+      redirect_to root_path, notice: "Request submitted successfully!"
+    else
+      @districts = District.all
+      @counties = @request.district.present? ? County.where(district_id: @request.district_id) : County.none
+      @sub_counties = @request.county.present? ? SubCounty.where(county_id: @request.county_id) : SubCounty.none
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(
+      :name, :phone_number, :request_type, :residence_address, 
+      :is_selected, :district_id, :county_id, :sub_county_id, 
+      :branch_id
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:registrations]
 
-  root 'home#index'
+  root "home#index"
+  post "create_request", to: "home#create_request"
   get 'home/index'
   post  'ussd_request', to: 'requests#ussd'
   resources :users

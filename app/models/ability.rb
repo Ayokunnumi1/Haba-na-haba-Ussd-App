7a93d79b9e301ef50882cbaec94ebc78e1e36c7f
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-11 16:27:21 +0600

    CanCanCan Ability setup

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,32 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    # Define abilities based on user role
+    user ||= User.new  # guest user (not logged in)
+
+    case user.role
+    when 'super_admin'
+      # Super admin can do everything
+      can :manage, :all
+    when 'admin'
+      # Admin can manage all except users and roles
+      can :manage, :all
+      cannot :manage, User
+    when 'branch_manager'
+      # Branch managers can only manage resources within their branch
+      can :manage, Request, branch_id: user.branch_id
+      can :read, Event
+      can :update, User, branch_id: user.branch_id
+      # If branch manager is authorized to read events assigned to their branch
+      can :read, EventUser, user_id: user.id
+    when 'volunteer'
+      # Volunteers can read events and requests
+      can :read, Event
+      can :read, Request, branch_id: user.branch_id
+    else
+      # Default to no access for users without a valid role
+      cannot :manage, :all
+    end
   end
 end

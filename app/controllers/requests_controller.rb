a939f8129c21bad44e865b68532e4f52d22c1240
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-11 15:45:00 +0600

    linters

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -39,7 +39,7 @@ class RequestsController < ApplicationController
     @counties = @request.district.present? ? County.where(district_id: @request.district_id) : County.none
     @sub_counties = @request.county.present? ? SubCounty.where(county_id: @request.county_id) : SubCounty.none
     @users = User.where(role: 'volunteer')
-  end 
+  end
 
   def create
     @request = Request.new(request_params)

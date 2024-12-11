3e3d334430636eab3164f2c31dcf76d85fb3051c
Ayokunnumi1 <omololuayk@gmail.com>
2024-12-12 00:54:29 +0100

    Use switch case to determine request type

--- app/helpers/other_donation_module.rb
+++ app/helpers/other_donation_module.rb
@@ -6,7 +6,29 @@ module OtherDonationModule
     district_name = input_parts[2]
     county_name = input_parts[3]
     sub_county_name = input_parts[4]
-    donation_type_selection = input_parts[5]
+    donation_type_input = input_parts[5]
+
+    # Determine the request type based on user input
+    request_type = case donation_type_input
+                   when '1'
+                     'cash_donation'
+                   when '2'
+                     'cloth_donation'
+                   when '3'
+                     'other_donation'
+                   else
+                     return 'END Invalid donation type selected.'
+                   end
+    
+    # Map the donation type input to human-readable values
+    donation_type_human_readable = case donation_type_input
+                                   when '1'
+                                     'Cash'
+                                   when '2'
+                                     'Clothing'
+                                   when '3'
+                                     'Other items'
+                                   end
 
     # Locate the district and branch
     selected_district = District.search_by_name(district_name).first
@@ -17,31 +39,20 @@ module OtherDonationModule
     return 'END No matching county found.' if selected_county.nil?
     return 'END No matching Sub county found.' if selected_sub_county.nil?
 
-    
-    donation_type_mapping = {
-      '1' => 'Cash Donation',
-      '2' => 'Cloth Donation',
-      '3' => 'Other Items Donation'
-    }
-
-    donation_request_type = donation_type_mapping[donation_type_selection]
-    puts "#{donation_request_type}, type donation"
-    
     branch = Branch.joins(:districts).find_by(districts: { id: selected_district.id })
     return 'END No branch found for the selected district and county.' if branch.nil?
 
-   
     Request.create(
       phone_number:,
       name: request_name,
-      request_type: donation_request_type,
+      request_type: request_type,
       district_id: selected_district.id,
       county_id: selected_county.id,
       sub_county_id: selected_sub_county.id,
       branch_id: branch.id
     )
 
-    message = "Thank you for your donation we are reaching out to you shortly. Proceed to branch #{branch.name} in #{selected_district.name}"
+    message = "Thank you for your #{donation_type_human_readable} donation. We are reaching out to you shortly. Proceed to branch #{branch.name} in #{selected_district.name}."
     SmsHelper.send_sms(phone_number, message)
     'END Thank you for your donation. We are processing your request and will call you shortly.'
   end

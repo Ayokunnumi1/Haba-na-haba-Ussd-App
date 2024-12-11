18c4ab5079c527b0c467c035f5fdef6d5dbae8b4
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-11 15:57:40 +0600

    linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,5 @@ module ApplicationHelper
       concat content_tag(:h3, title, class: 'font-bold text-xl lg:text-3.8xl lg:mt-4')
       concat content_tag(:p, description, class: 'font-normal text-sm lg:text-base lg:mt-4')
     end
-    def display_navbar?
-      users_actions = %w[new edit show]
-      requests_actions = %w[new edit show]
-
-      controller_name == 'users' && action_name.in?(users_actions) &&
-        !(controller_name == 'requests' && action_name.in?(requests_actions))
-    end
   end
 end

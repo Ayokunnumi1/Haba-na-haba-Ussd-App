fd9431a62f26e244e0eaac0af4eec219c8a4dabb
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-12 18:31:41 +0600

    admin stage cancan starting point

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,29 +4,35 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    case user.role
+    when 'super_admin'
+      can :manage, :all
+    when 'admin'
+      can :manage, :all
+      cannot [:update, :destroy], User, role: 'admin'
+      cannot [:create, :update, :destroy], User, role: 'super_admin' 
+    when 'branch_manager'
+      can :manage, User
+      can :read, Branch
+      can [:create, :update], Branch, id: user.branch_id
+      cannot [:create, :destroy], Branch
+      can :manage, Request, branch_id: user.branch_id
+
+      cannot [:update, :destroy], User, role: 'admin'
+      cannot [:create, :update, :destroy], User, role: 'super_admin' 
+
+      # Branch managers can create and update volunteers in their branch
+      can [:create, :update], User, role: 'volunteer'
+
+      # Branch managers cannot edit or delete other branch managers, admins, or super admins
+      cannot [:create, :update, :destroy], User, role: %w[branch_manager admin super_admin]
+
+      # Branch managers can only update their own profile
+      can :update, User, id: user.id
+    when 'volunteer'
+      # Volunteers can read events and requests
+      can :read, Event
+      can :read, Request, branch_id: user.branch_id
+    end
   end
 end

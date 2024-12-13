f2309330b4a718872db2f6f9d532f9181f838afd
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-13 23:52:33 +0600

    update ability linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,43 +2,69 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+
     case user.role
     when 'volunteer'
-      can %i[create update read], Request, user_id: user.id
-      can %i[create update read], Inventory
-      can %i[create update read], IndividualBeneficiary
-      can %i[create update read], FamilyBeneficiary
-      can %i[create update read], OrganizationBeneficiary
-      can :read, User
-      can :read, Branch
-      can :read, Event
-      can :read, District
-      can :update, User, id: user.id
+      volunteer_permissions(user)
     when 'branch_manager'
-      can :read, User
-      can :read, Branch
-      can %i[create update], Branch, id: user.branch_id
-      cannot %i[create destroy], Branch
-      can :manage, User, role: 'volunteer'
-      cannot :update, User, role: 'volunteer'
-      cannot %i[create update destroy], User, role: %i[branch_manager admin super_admin]
-      can :manage, Request, branch_id: user.branch_id
-      can :manage, Inventory, branch_id: user.branch_id
-      can :manage, IndividualBeneficiary, branch_id: user.branch_id
-      can :manage, FamilyBeneficiary, branch_id: user.branch_id
-      can :manage, OrganizationBeneficiary, branch_id: user.branch_id
-      can :manage, Event
-      cannot :destroy, Event
-      can :manage, District
-      cannot :destroy, District
-      can :update, User, id: user.id
+      branch_manager_permissions(user)
     when 'admin'
-      can :manage, :all
-      cannot %i[update destroy], User, role: 'admin'
-      cannot %i[create update destroy], User, role: 'super_admin'
-      can :update, User, id: user.id
+      admin_permissions(user)
     when 'super_admin'
-      can :manage, :all
+      super_admin_permissions
+    else
+      guest_permissions
     end
+  end
+
+  private
+
+  def volunteer_permissions(user)
+    can %i[create update read], Request, user_id: user.id
+    can %i[create update read], Inventory
+    can %i[create update read], IndividualBeneficiary
+    can %i[create update read], FamilyBeneficiary
+    can %i[create update read], OrganizationBeneficiary
+    can :read, User
+    can :read, Branch
+    can :read, Event
+    can :read, District
+    can :update, User, id: user.id
+  end
+
+  def branch_manager_permissions(user)
+    can :read, User
+    can :read, Branch
+    can %i[create update], Branch, id: user.branch_id
+    cannot %i[create destroy], Branch
+    can :manage, User, role: 'volunteer'
+    cannot :update, User, role: 'volunteer'
+    cannot %i[create update destroy], User, role: %i[branch_manager admin super_admin]
+    can :manage, Request, branch_id: user.branch_id
+    can :manage, Inventory, branch_id: user.branch_id
+    can :manage, IndividualBeneficiary, branch_id: user.branch_id
+    can :manage, FamilyBeneficiary, branch_id: user.branch_id
+    can :manage, OrganizationBeneficiary, branch_id: user.branch_id
+    can :manage, Event
+    cannot :destroy, Event
+    can :manage, District
+    cannot :destroy, District
+    can :update, User, id: user.id
+  end
+
+  def admin_permissions(user)
+    can :manage, :all
+    cannot %i[update destroy], User, role: 'admin'
+    cannot %i[create update destroy], User, role: 'super_admin'
+    can :update, User, id: user.id
+  end
+
+  def super_admin_permissions
+    can :manage, :all
+  end
+
+  def guest_permissions
+    cannnot :manage, :all
   end
 end

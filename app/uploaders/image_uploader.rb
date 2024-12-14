e88ee235428e2043f1c9bdea20cd3361ac4458d9
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-12-14 23:24:40 +0600

    added image optimizer

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,15 @@
+# app/uploaders/image_uploader.rb
+class ImageUploader < CarrierWave::Uploader::Base
+  include ImageProcessing::MiniMagick
+
+  process :convert_to_webp
+
+  def convert_to_webp
+    manipulate! do |img|
+      img.format('webp') do |c|
+        c.quality '80'
+      end
+      img
+    end
+  end
+end

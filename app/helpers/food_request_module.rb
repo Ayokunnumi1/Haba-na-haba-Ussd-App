05fd58d1e8a315f7e2028b27548037bace78886a
Austin Obimbo <obimboausts@gmail.com>
2024-12-16 21:38:51 +0300

    update food request module and remover county, subcounty inputs

--- app/helpers/food_request_module.rb
+++ app/helpers/food_request_module.rb
@@ -5,32 +5,20 @@ module FoodRequestModule
     input_parts = text.split('*')
     request_name = input_parts[1]
     district_name = input_parts[2]
-    county_name = input_parts[3]
-    sub_county_name = input_parts[4]
 
     # Find the district by name
-    selected_district = District.search_by_name(district_name).first
+    selected_district = District.search_by_name(district_name).first || District.find_by(name: 'Default District')
     return 'END No matching district found.' unless selected_district
-
-    # Find the county within the district
-    selected_county = selected_district.counties.search_by_name(county_name).first
-    return 'END No matching county found for the selected district.' unless selected_county
-
-    # Find the sub-county within the county
-    selected_sub_county = selected_county.sub_counties.search_by_name(sub_county_name).first
-    return 'END No matching sub-county found for the selected county.' unless selected_sub_county
-
+    
     # Find the branch associated with the district
-    branch = Branch.joins(:districts).find_by(districts: { id: selected_district.id })
+    branch = Branch.joins(:districts).find_by(districts: { id: selected_district.id }) || Branch.find_by(name: 'Default Branch')
     return 'END No branch found for the selected district.' unless branch
-
-    branch_name = branch.name
 
     # Create a new request
     new_request = Request.new(
       phone_number:,
       name: request_name,
-      request_type: 'food_request',
+      request_type: 'food_request',                                 
       district_id: selected_district.id,
       county_id: selected_county.id,
       sub_county_id: selected_sub_county.id,

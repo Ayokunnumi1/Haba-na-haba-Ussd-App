dc351f8ad92c30afd8c7780b0e74f0d531676cfc
Austin Obimbo <obimboausts@gmail.com>
2024-12-16 21:47:05 +0300

    updated other request module and added  donation amount

--- app/helpers/other_donation_module.rb
+++ app/helpers/other_donation_module.rb
@@ -19,9 +19,9 @@ module OtherDonationModule
     input_parts = text.split('*')
     request_name = input_parts[1]
     district_name = input_parts[2]
-    county_name = input_parts[3]
-    sub_county_name = input_parts[4]
-    donation_type_input = input_parts[5]
+    donation_type_input = input_parts[3]
+    donation_amount = input_parts[4]
+
 
     # Determine the request type based on user input
     request_type = DONATION_TYPE_MAP[donation_type_input]
@@ -31,24 +31,18 @@ module OtherDonationModule
     donation_type_human_readable = DONATION_TYPE_HUMAN_READABLE_MAP[donation_type_input]
 
     # Locate the district and branch
-    selected_district = District.search_by_name(district_name).first
-    selected_county = selected_district.counties.search_by_name(county_name).first
-    selected_sub_county = selected_county.sub_counties.search_by_name(sub_county_name).first
-
+    selected_district = District.search_by_name(district_name).first || District.find_by(name: 'Default District')
     return 'END No matching district found.' if selected_district.nil?
-    return 'END No matching county found.' if selected_county.nil?
-    return 'END No matching Sub county found.' if selected_sub_county.nil?
-
-    branch = Branch.joins(:districts).find_by(districts: { id: selected_district.id })
+    
+    branch = Branch.joins(:districts).find_by(districts: { id: selected_district.id }) || Branch.find_by(name: 'Haba na Haba Branch')
     return 'END No branch found for the selected district and county.' if branch.nil?
 
     Request.create(
       phone_number:,
       name: request_name,
       request_type: request_type,
+      amount: donation_amount,
       district_id: selected_district.id,
-      county_id: selected_county.id,
-      sub_county_id: selected_sub_county.id,
       branch_id: branch.id
     )
 

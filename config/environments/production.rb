3fa2fb5d9b1811b24193ac015b1037414740b5b8
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-05-07 15:33:42 +0600

    test new congfig  production passenger phusion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.assets.css_compressor = nil
-  #Md Mahadi Hasan
-  config.assets.js_compressor = nil
 
   
   # Code is not reloaded between requests.
@@ -33,7 +31,9 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.digest = true
+  config.public_file_server.enabled = true
+  
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 

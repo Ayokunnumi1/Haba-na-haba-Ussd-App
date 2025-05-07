1a338f4b79741529b1271db8ba2fca3bb62132db
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-05-07 15:12:35 +0600

    name path changed

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -74,7 +74,7 @@ module.exports = {
         "users-image": "url('/assets/users-image.svg')",
         "users-show": "url('/assets/users-show.svg')",
         "login-bg": "url('/assets/Shape.svg')",
-        "custom-image": "url('/assets/african-kid.png')",
+        "custom-image": "url('/assets/kid.png')",
         "community-image": "url('/assets/community.png')",
       },
       screens: {

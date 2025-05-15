cd874496003c19bc889b4569e142961b9ccc7adc
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-05-16 02:28:51 +0600

    fix district js with the code from develope

--- app/javascript/district.js
+++ app/javascript/district.js
@@ -2,9 +2,6 @@ document.addEventListener("turbo:load", () => {
   const toggleButton = document.getElementById("edit-district-toggle");
   const formContainer = document.getElementById("edit-district-form");
 
-  // Only proceed if both elements exist
-  if (!toggleButton || !formContainer) return;
-  
   toggleButton.addEventListener("click", (event) => {
     event.preventDefault(); // Prevent the default link action
     formContainer.classList.toggle("hidden"); // Toggle the visibility
